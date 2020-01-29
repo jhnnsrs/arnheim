20429ff56b68e636e5a134cb6451912b6c1ecc1d
jhnnsrs <jhnnsrs@gmail..com>
2020-01-29 14:53:04 +0100

    Added Notebooks and Migrations

---
+++ bergen/metamorphers/nodes.py
@@ -0,0 +1,13 @@
+from drawing.models import ROI
+from larvik.discover import register_node, NodeType
+from metamorphers.models import Display
+
+
+@register_node("twodshow")
+class TwoDShow(NodeType):
+    inputs = [Display]
+    outputs = [ROI]
+    type = "show"
+    settings = {"reload": True}
+    name = "Two D Show"
+    path = "TwoDShow"
